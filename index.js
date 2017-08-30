47a7e80d889c02f7235632e0742f358bfc3eef73
jilleborg <jborg@guidion.net>
2017-08-30 10:19:29 +0200

    chore(): misc

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-var form_module_1 = require("./src/form/form.module");
-exports.FormBuilderModule = form_module_1.FormBuilderModule;
+__export(require("./src/form/form.module"));
 var form_1 = require("./src/form/form");
 exports.FormConfig = form_1.FormConfig;
